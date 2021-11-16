600ba6e1fa0ac89f2c706487e75693e1aac1c054
SuperProfessional <nguyenhoan.world@gmail.com>
2021-11-16 23:59:33 +0700

    GUI_mainFrame
    giao diện main frame và chia pakege

--- src/views/main_frame.java
+++ src/views/main_frame.java
@@ -54,8 +54,9 @@ public class main_frame extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         pnl5 = new javax.swing.JPanel();
         jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
         jSeparator4 = new javax.swing.JSeparator();
+        btnScanQR = new javax.swing.JButton();
+        jLabel7 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         lblClock = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
@@ -82,7 +83,7 @@ public class main_frame extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Quản lý thư viện");
-        setPreferredSize(new java.awt.Dimension(900, 650));
+        setPreferredSize(new java.awt.Dimension(900, 800));
         setResizable(false);
 
         pnlMain.setBackground(new java.awt.Color(255, 255, 255));
@@ -98,6 +99,7 @@ public class main_frame extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images_Icon/book.png"))); // NOI18N
         jLabel1.setText("Quản lý sách");
         jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
@@ -109,7 +111,7 @@ public class main_frame extends javax.swing.JFrame {
         });
         pnl1.add(jLabel1, java.awt.BorderLayout.CENTER);
 
-        pnlWest.add(pnl1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 170, 250, 45));
+        pnlWest.add(pnl1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 220, 250, 45));
 
         pnl2.setBackground(new java.awt.Color(3, 120, 170));
         pnl2.setLayout(new java.awt.BorderLayout());
@@ -128,7 +130,7 @@ public class main_frame extends javax.swing.JFrame {
         });
         pnl2.add(jLabel2, java.awt.BorderLayout.CENTER);
 
-        pnlWest.add(pnl2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 220, 250, 45));
+        pnlWest.add(pnl2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 270, 250, 45));
 
         pnl3.setBackground(new java.awt.Color(3, 120, 170));
         pnl3.setLayout(new java.awt.BorderLayout());
@@ -136,6 +138,7 @@ public class main_frame extends javax.swing.JFrame {
         jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images_Icon/members .png"))); // NOI18N
         jLabel3.setText("Quản lý hội viên");
         jLabel3.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
@@ -147,7 +150,7 @@ public class main_frame extends javax.swing.JFrame {
         });
         pnl3.add(jLabel3, java.awt.BorderLayout.CENTER);
 
-        pnlWest.add(pnl3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 270, 250, 45));
+        pnlWest.add(pnl3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 320, 250, 45));
 
         pnl4.setBackground(new java.awt.Color(3, 120, 170));
         pnl4.setLayout(new java.awt.BorderLayout());
@@ -166,7 +169,7 @@ public class main_frame extends javax.swing.JFrame {
         });
         pnl4.add(jLabel4, java.awt.BorderLayout.CENTER);
 
-        pnlWest.add(pnl4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 320, 250, 45));
+        pnlWest.add(pnl4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 370, 250, 45));
 
         pnl5.setBackground(new java.awt.Color(3, 120, 170));
         pnl5.setLayout(new java.awt.BorderLayout());
@@ -185,16 +188,20 @@ public class main_frame extends javax.swing.JFrame {
         });
         pnl5.add(jLabel5, java.awt.BorderLayout.CENTER);
 
-        pnlWest.add(pnl5, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 380, 250, 45));
-
-        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
-        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel6.setText("Library");
-        pnlWest.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 250, 100));
+        pnlWest.add(pnl5, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 430, 250, 45));
 
         jSeparator4.setForeground(new java.awt.Color(255, 255, 255));
-        pnlWest.add(jSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 370, 250, -1));
+        pnlWest.add(jSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 420, 250, -1));
+
+        btnScanQR.setBackground(new java.awt.Color(0, 153, 0));
+        btnScanQR.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        btnScanQR.setForeground(new java.awt.Color(255, 255, 255));
+        btnScanQR.setText("Scan QR");
+        pnlWest.add(btnScanQR, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 520, 170, 50));
+
+        jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images_Icon/library.png"))); // NOI18N
+        pnlWest.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, 250, 110));
 
         pnlMain.add(pnlWest, java.awt.BorderLayout.WEST);
 
@@ -372,12 +379,13 @@ public class main_frame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnScanQR;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPopupMenu.Separator jSeparator1;
