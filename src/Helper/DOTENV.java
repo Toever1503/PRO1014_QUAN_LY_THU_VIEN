e04c9ce1e10cdd7ac80c29735fff71acde909721
Toever1503 <haunvph13497@fpt.edu.vn>
2021-11-19 18:41:11 +0700

    Code ket noi sql
    tao class dotnev lay cau hinh sql
    va class connection lay ket noi

---
+++ src/Helper/DOTENV.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Helper;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+/**
+ *
+ * @author haunv
+ *
+ */
+public class DOTENV {
+    public static Dotenv getEnv(String file){ // method to get .env file return null if file not found else Dotenv object
+        Dotenv dotenv = Dotenv.configure().filename(file).ignoreIfMissing().ignoreIfMalformed().load();
+        if(dotenv == null) return null;
+        return dotenv;
+    }
+
+}
