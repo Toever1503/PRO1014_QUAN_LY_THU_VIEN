6b3a5680b276a49857e05e84da0e900ac8ede1bf
Toever1503 <haunvph13497@fpt.edu.vn>
2021-11-24 11:51:22 +0700

    update xong TacGiaDao va TheLoaiDao
    đã update xong 2  class

--- src/DAO/TacGiaDao.java
+++ src/DAO/TacGiaDao.java
@@ -21,36 +21,80 @@ import java.util.logging.Logger;
  */
 public class TacGiaDao extends DAO<TacGia, BigInteger> {
 
-    private final String SELECT_ALL_BY_PAGE_SQL = "";
-    private final String SELECT_BY_ID_SQL = "";
-    private final String INSERT_ON_UPDATE_SQL = "";
-    private final String DELETE_SQL = "";
-    
+    private final String SELECT_ALL_BY_PAGE_SQL = "SELECT `ID`, `TenTacGia` FROM `tac_gia` LIMIT ?, 30";
+    private final String SELECT_BY_ID_SQL = "SELECT `ID`, `TenTacGia` FROM `tac_gia` WHERE ID = ?";
+    private final String INSERT_ON_UPDATE_SQL = "INSERT INTO `tac_gia` (`ID`, `TenTacGia`) VALUES (?, ?)\n"
+            + "ON DUPLICATE KEY UPDATE TenTacGia=VALUES(TenTacGia)";
+    private final String DELETE_SQL = "DELETE FROM `tac_gia` WHERE ID = ?";
+
     private static TacGiaDao instance;
 
-    @Override
-    public int save(TacGia entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private TacGiaDao() {
+    }
+
+    public static TacGiaDao getInstance() {
+        if (instance == null) {
+            instance = new TacGiaDao();
+        }
+        return instance;
     }
 
     @Override
-    public int delete(BigInteger id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int save(TacGia entity
+    ) {
+        int row = 0;
+        try {
+            row = Helper.Utility.update(INSERT_ON_UPDATE_SQL,
+                    entity.getId(),
+                    entity.getTenTacGia());
+        } catch (Exception ex) {
+            Logger.getLogger(PhieuMuonDao.class.getName()).log(Level.SEVERE, "Insert TacGia Failed", ex);
+        }
+        return row;
     }
 
     @Override
-    public TacGia selectByID(BigInteger id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int delete(BigInteger id
+    ) {
+        int row = 0;
+        try {
+            row = Helper.Utility.update(DELETE_SQL, id);
+        } catch (Exception ex) {
+            Logger.getLogger(PhieuMuonDao.class.getName()).log(Level.SEVERE, "Delete TacGia Failed", ex);
+        }
+        return row;
     }
 
     @Override
-    public List<TacGia> selectAllByPage(int page) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public TacGia selectByID(BigInteger id
+    ) {
+        List<TacGia> list = this.selectBySql(SELECT_BY_ID_SQL, id);
+        return list == null ? null : list.get(0);
     }
 
     @Override
-    protected List<TacGia> selectBySql(String sql, Object... args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<TacGia> selectAllByPage(int page
+    ) {
+        List<TacGia> list = this.selectBySql(SELECT_ALL_BY_PAGE_SQL, page);
+        return list == null ? null : list;
     }
 
+    @Override
+    protected List<TacGia> selectBySql(String sql, Object... args
+    ) {
+        List<TacGia> list = new ArrayList<TacGia>();
+
+        try {
+            ResultSet rs = Helper.Utility.query(sql, args);
+            while (rs.next()) {
+                list.add(new TacGia(
+                        rs.getObject("ID", BigInteger.class),
+                        rs.getNString("TenTacGia")));
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(PhieuMuonDao.class.getName()).log(Level.SEVERE, "Select TacGia Failed", ex);
+        }
+        return list.size() == 0 ? null : list;
+    }
+    
 }
--- src/DAO/TheLoaiDao.java
+++ src/DAO/TheLoaiDao.java
@@ -22,37 +22,76 @@ import java.util.logging.Logger;
  */
 public class TheLoaiDao extends DAO<TheLoai, BigInteger> {
 
-    private final String SELECT_ALL_BY_PAGE_SQL = "";
-    private final String SELECT_BY_ID_SQL = "";
-    private final String INSERT_ON_UPDATE_SQL = "";
-    private final String DELETE_SQL = "";
-    private static TheLoaiDao theLoaiDao;
+    private final String SELECT_ALL_BY_PAGE_SQL = "SELECT `ID`, `TenTheLoai` FROM `the_loai` LIMIT ?, 30";
+    private final String SELECT_BY_ID_SQL = "SELECT `ID`, `TenTheLoai` FROM `the_loai` WHERE ID = ?";
+    private final String INSERT_ON_UPDATE_SQL = "INSERT INTO `the_loai` (`ID`, `TenTheLoai`) VALUES (?, ?)\n"
+            + "ON DUPLICATE KEY UPDATE TenTheLoai=VALUES(TenTheLoai)";
+    private final String DELETE_SQL = "DELETE FROM `the_loai` WHERE ID = ?";
 
     private static TheLoaiDao instance;
 
+    private TheLoaiDao() {
+    }
+
+    public static TheLoaiDao getInstance() {
+        if (instance == null) {
+            instance = new TheLoaiDao();
+        }
+        return instance;
+    }
+
     @Override
     public int save(TheLoai entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int row = 0;
+        try {
+            row = Helper.Utility.update(INSERT_ON_UPDATE_SQL,
+                    entity.getId(),
+                    entity.getTenTheLoai());
+        } catch (Exception ex) {
+            Logger.getLogger(PhieuMuonDao.class.getName()).log(Level.SEVERE, "Insert TheLoai Failed", ex);
+        }
+        return row;
     }
 
     @Override
     public int delete(BigInteger id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int row = 0;
+        try {
+            row = Helper.Utility.update(DELETE_SQL, id);
+        } catch (Exception ex) {
+            Logger.getLogger(PhieuMuonDao.class.getName()).log(Level.SEVERE, "Delete TheLoai Failed", ex);
+        }
+        return row;
     }
 
     @Override
     public TheLoai selectByID(BigInteger id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<TheLoai> list = this.selectBySql(SELECT_BY_ID_SQL, id);
+        return list == null ? null : list.get(0);
     }
 
     @Override
     public List<TheLoai> selectAllByPage(int page) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<TheLoai> list = this.selectBySql(SELECT_ALL_BY_PAGE_SQL, page*30);
+        return list == null ? null : list;
     }
 
     @Override
     protected List<TheLoai> selectBySql(String sql, Object... args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<TheLoai> list = new ArrayList<TheLoai>();
+
+        try {
+            ResultSet rs = Helper.Utility.query(sql, args);
+            while (rs.next()) {
+                list.add(new TheLoai(
+                        rs.getObject("ID", BigInteger.class),
+                        rs.getNString("TenTheLoai")));
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(PhieuMuonDao.class.getName()).log(Level.SEVERE, "Select TheLoai Failed", ex);
+        }
+        return list.size() == 0 ? null : list;
     }
+    
 
 }
