fa57c9b15ad75c37b600296dc364be930fa40798
SuperProfessional <94169756+SuperProfessional@users.noreply.github.com>
2021-11-24 21:30:05 +0700

    Revert ""NhaXuatBanDAO_PhieuMuonDAO""

--- src/Models/PhieuMuon.java
+++ src/Models/PhieuMuon.java
@@ -4,21 +4,25 @@
  */
 package Models;
 
-import java.math.BigInteger;
 import java.sql.Date;
+
 /**
  *
  * @author haunv
  */
 public class PhieuMuon {
-    private BigInteger id;
-    private BigInteger nguoiMuon;
-    private BigInteger nguoiXuLy;
+
+    private long id;
+    private long nguoiMuon;
+    private String nguoiXuLy;
     private Date ngayMuon;
     private Date hanTra;
     private String qr_code;
 
-    public PhieuMuon(BigInteger id, BigInteger nguoiMuon, BigInteger nguoiXuLy, Date ngayMuon, Date hanTra, String qr_code) {
+    public PhieuMuon() {
+    }
+
+    public PhieuMuon(long id, long nguoiMuon, String nguoiXuLy, Date ngayMuon, Date hanTra, String qr_code) {
         this.id = id;
         this.nguoiMuon = nguoiMuon;
         this.nguoiXuLy = nguoiXuLy;
@@ -27,27 +31,27 @@ public class PhieuMuon {
         this.qr_code = qr_code;
     }
 
-    public BigInteger getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(BigInteger id) {
+    public void setId(long id) {
         this.id = id;
     }
 
-    public BigInteger getNguoiMuon() {
+    public long getNguoiMuon() {
         return nguoiMuon;
     }
 
-    public void setNguoiMuon(BigInteger nguoiMuon) {
+    public void setNguoiMuon(long nguoiMuon) {
         this.nguoiMuon = nguoiMuon;
     }
 
-    public BigInteger getNguoiXuLy() {
+    public String getNguoiXuLy() {
         return nguoiXuLy;
     }
 
-    public void setNguoiXuLy(BigInteger nguoiXuLy) {
+    public void setNguoiXuLy(String nguoiXuLy) {
         this.nguoiXuLy = nguoiXuLy;
     }
 
@@ -74,4 +78,10 @@ public class PhieuMuon {
     public void setQr_code(String qr_code) {
         this.qr_code = qr_code;
     }
+
+    @Override
+    public String toString() {
+        return "PhieuMuon{" + "id=" + id + ", nguoiMuon=" + nguoiMuon + ", nguoiXuLy=" + nguoiXuLy + ", ngayMuon=" + ngayMuon + ", hanTra=" + hanTra + ", qr_code=" + qr_code + '}';
+    }
+
 }
